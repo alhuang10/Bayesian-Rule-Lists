e2837d3342c921971af4cab91cf03e6f71e26c83
Sidd Seethepalli <siddseethepalli@gmail.com>
2017-04-27 18:54:54 -0400

    Modify hierarchical prior to use more general classes from utils

---
+++ brl/utils.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+
+class Expression(object):
+    def __init__(self, index, op, value):
+        self.index = index
+        self.op = op
+        self.value = value
+
+    def evaluate(self, x):
+        return self.op(x[self.index], self.value)
+
+class Antecedent(object):
+    def __init__(self, expressions):
+        self.expressions = expressions
+
+    def evaluate(self, x):
+        return all(exp.evaluate(x) for exp in expressions)
+
+    def length(self):
+        return len(self.expressions)
+
+class Consequent(object):
+    def __init__(self, probabilities):
+        self.probabilities = probabilities
+
+    def evaluate(self, y):
+        return self.probabilities[y]
+
+class IfThenBlock(object):
+    def __init__(self, antecedent, consequent):
+        self.antecedent = antecedent
+        self.consequent = consequent
+
+    def checkIf(self, x):
+        return self.antecedent.evaluate(x)
+
+    def evaluate(self, y):
+        return self.consequent.evaluate(y)
+
+    def getAntecedent(self):
+        return self.antecedent
+
+class BayesianRuleList(object):
+    def __init__(self, ifThenBlocks, defaultConsequent):
+        self.ifThenBlocks = ifThenBlocks
+        self.defaultConsequent = defaultConsequent
+
+    def length(self):
+        return len(self.ifThenBlocks)
+
+    def getAntecedentByIndex(self, idx):
+        return self.ifThenBlocks[idx].getAntecedent()
+
+class AntecedentGroup(object):
+    def __init__(self, antecedents):
+        self.antecedentsBySize = defaultdict(list)
+        for antecedent in antecedents:
+            self.antecedentsBySize[antecedent.length()].append(antecedent)
+
+    def sizes(self):
+        return self.antecedentsBySize.keys()
+
+    def lengthsBySize(self):
+        return {k: len(v) for k, v in self.antecedentsBySize.items()}
--- generative_model.py
+++ generative_model.py
@@ -2,47 +2,51 @@ import math
 
 import pandas as pd
 
-def getNumAntecedents(A):
-    count = 0
-    for size in A:
-        count += len(A[size])
-    return count
-
-# p(m|A,lmda)
-def p_m(m, A, lmda):
-    return (lmda ** m + 0.0) / (math.factorial(m) * math.e ** m)
-
-# product from i=1 to m of p(c_j|c_1,...c_{j-1},A,eta)
-def p_c(d, A, eta):
-    normalizer = 0
-    for size in A:
-        normalizer += eta ** size / math.factorial(size)
-
-    prod = 1.0
-    for i in range(len(d)):
-        prod *= eta ** len(d[i]) / (math.factorial(len(d[i])) * normalizer)
-    return prod
-
-# product from i=1 to m of p(a_j|a_1,...a_{j-1},A)
-def p_a(d, A):
-    sizes = {}
-    for size in A:
-        sizes[size] = 0
-
-    prod = 1.0
-    for i in range(len(d)):
-        prod *= 1.0 / (len(A[len(d[i])]) - sizes[len(d[i])])
-        sizes[len(d[i])] += 1
-    return prod
-
-# p(d|A,lmda,eta)
-def p_d(d, A, lmda, eta):
-    return p_m(len(d), A, lmda) * p_c(d, A, eta) * p_a(d, A)
+OPTIMIZATION_THRESHOLD = 10
 
 # p(y|x,d,alpha)
 def p_y(y, x, d, alpha):
     pass
 
-# p(d|x,y,A,alpha,lmda,eta)
-def p_d_given_data(d, x, y, A, alpha, lmda, eta):
-    return p_d(d, A, lmda, eta) * p_y(y, x, d, alpha)
+# product from i=1 to m of p(a_j|a_1,...a_{j-1},a)
+def p_a(d, a):
+    sizes = {}
+    for size in a.sizes():
+        sizes[size] = 0
+
+    prod = 1.0
+    for i in range(d.length()):
+        antLen = d.getAntecedentByIndex(i).length()
+        prod *= 1.0 / (a.lengthsBySize[antLen] - sizes[antLen])
+        sizes[antLen] += 1
+    return prod
+
+# product from i=1 to m of p(c_j|c_1,...c_{j-1},a,eta)
+def p_c(d, a, eta):
+    normalizer = 0
+    for size in a:
+        normalizer += eta ** size / math.factorial(size)
+
+    prod = 1.0
+    for i in range(d.length()):
+        antLen = d.getAntecedentByIndex(i).length()
+        prod *= (eta ** antLen / (math.factorial(antLen) * normalizer))
+    return prod
+
+# p(m|A,lmda)
+def p_m(m, a, lmda):
+    if a.length() >= OPTIMIZATION_THRESHOLD * m
+        return (lmda ** m + 0.0) / (math.factorial(m) * math.e ** m)
+
+    den = 0
+    for i in range(a.length() + 1):
+        den += (lmda ** i + 0.0) / (math.factorial(i))
+    return (lmda ** m + 0.0) / (math.factorial(m) * den)
+
+# p(d|a,lmda,eta)
+def p_d(d, a, lmda, eta):
+    return p_m(d.length(), a, lmda) * p_c(d, a, eta) * p_a(d, a)
+
+# p(d|x,y,a,alpha,lmda,eta)
+def p_d_given_data(d, x, y, a, alpha, lmda, eta):
+    return p_d(d, a, lmda, eta) * p_y(y, x, d, alpha)
